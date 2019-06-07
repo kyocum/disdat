69f37200dbeb6ebe8f70170965f9eefff4f6b291
Kenneth <kyocum@users.noreply.github.com>
2019-06-07 16:26:37 -0700

    Add failure conditions to git hash extraction  (#108)
    * Added ability to handle failures when we check for git hash of files added through api.add
    
    * adding comment
    
    * bundle api close now makes no assumptions on git hash
    
    * Made lineage printing more robust

--- tests/functional/test_add.py
+++ tests/functional/test_add.py
@@ -314,3 +314,8 @@ def test_data_as_bundle_not_csv(tmpdir):
     assert api.get(TEST_CONTEXT, 'test_file_as_bundle_txt_file') is None, 'Bundle should not exist'
 
     api.delete_context(TEST_CONTEXT)
+
+
+if __name__ == '__main__':
+    import tempfile
+    test_single_file(tempfile.gettempdir())
