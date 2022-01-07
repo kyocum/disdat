8a1350e4a3f133109329adda856579a0280c5fd6
Ken Yocum <kyocum@gmail.com>
2022-01-06 20:23:35 -0800

    remove unneeded functions in common.py

--- disdat/common.py
+++ disdat/common.py
@@ -21,13 +21,10 @@ import logging
 import os
 import sys
 import shutil
-import importlib
-import subprocess
 import uuid
 
 from six.moves import urllib
 from six.moves import configparser
-import six
 
 from disdat import resource
 import disdat.config
@@ -48,22 +45,6 @@ BUNDLE_TAG_PARAMS_PREFIX = '__param.'
 BUNDLE_TAG_TRANSIENT = '__transient'
 BUNDLE_TAG_PUSH_META = '__push_meta'
 
-LOCAL_EXECUTION = 'LOCAL_EXECUTION'  # Docker endpoint env variable if we're running a container locally
-
-
-class ApplyError(Exception):
-    def __init__(self, message, apply_result):
-        super(ApplyError, self).__init__(message)
-        self.apply_result = apply_result
-    @property
-    def result(self):
-        return self.apply_result
-
-
-class ExtDepError(Exception):
-    def __init__(self, message):
-        super(ExtDepError, self).__init__(message)
-
 
 class CatNoBundleError(Exception):
     def __init__(self, message):
@@ -78,45 +59,6 @@ class BadLinkError(Exception):
 def error(msg, *args, **kwargs):
     _logger.error(msg, *args, **kwargs)
     sys.exit(1)
-
-
-def apply_handle_result(apply_result, raise_not_exit=False):
-    """ Execute an appropriate sys.exit() call based on the dictionary
-    returned by apply.
-
-    Args:
-        apply_result(dict): Has keys 'success' and 'did_work' that give Boolean values.
-        raise_not_exit (bool): Raise ApplyException instead of performing sys.exit
-
-    Returns:
-        None
-
-    """
-
-    if apply_result['success']:
-        if raise_not_exit:
-            pass
-        else:
-            sys.exit(None) # None yields exit value of 0
-    else:
-        error_str = "Disdat Apply ran, but one or more tasks failed or missing dependencies."
-        if raise_not_exit:
-            raise ApplyError(error_str, apply_result)
-        else:
-            sys.exit(error_str)
-
-
-class SingletonType(type):
-    def __call__(self, *args, **kwargs):
-        try:
-            return self.__instance
-        except AttributeError:
-            self.__instance = super(SingletonType, self).__call__(*args, **kwargs)
-            return self.__instance
-
-
-class MySingleton(object):
-    __metaclass__ = SingletonType
 
 
 class DisdatConfig(object):
@@ -231,56 +173,6 @@ class DisdatConfig(object):
         dst = directory
         shutil.copytree(src, dst)
 
-#
-# subprocess wrapper
-#
-
-
-def do_subprocess(cmd, cli):
-    """ Standardize error processing
-
-    Args:
-        cmd (str): command to execute
-        cli (bool): whether called from CLI (True) or API (False)
-
-    Returns:
-        (int): 0 if success, >0 if failure
-
-    """
-    output = 'No captured output from running CMD [{}]'.format(cmd)
-    try:
-        if not cli:
-            output = subprocess.check_output(cmd)
-        else:
-            subprocess.check_call(cmd)
-    except subprocess.CalledProcessError as cpe:
-        if not cli:
-            print (output)
-            return cpe.returncode
-        raise
-
-    return 0
-
-
-def do_subprocess_with_output(cmd):
-    """ Standardize error processing
-
-    Args:
-        cmd (str): command to execute
-
-    Returns:
-        (str): output of command
-
-    """
-    try:
-        output = subprocess.check_output(cmd)
-        return output
-    except subprocess.CalledProcessError as cpe:
-        raise
-
-#
-# One place to update all the uuid's made by Disdat
-#
 
 def create_uuid():
     """
@@ -291,149 +183,6 @@ def create_uuid():
 
     """
     return str(uuid.uuid4())
-
-
-#
-# Make Docker images names from pipeline class names
-#
-
-
-def make_project_image_name(setup_file_path):
-    """
-    Create a container name from the name field in the setup.py file.
-    This uses some setuptools magic.  When you install disdat, we install
-    an entrypoint.   It becomes available to anyone using setup tools.
-    This extracts the information from the setup.py.
-
-    see disdat/infrastructure/dockerizer/setup_tools_commands.py
-
-    Args:
-        setup_file_path (str): The FQP to the setup.py file used to dockerize.
-
-    Returns:
-        (str): image name string
-
-    """
-
-    python_command = [
-        'python',
-        setup_file_path,
-        '-q',
-        'dsdt_distname'
-    ]
-
-    retval = do_subprocess_with_output(python_command).strip()
-
-    # If P3, this may be a byte array.   If P2, if not unicode, convert ...
-    retval = six.ensure_str(retval)
-
-    return retval
-
-
-def make_sagemaker_project_image_name(setup_file_path):
-    """ Create the string for the image for this pipeline if it uses sagemaker's
-    calling convention
-
-    Args:
-        setup_file_path (str): The FQP to the setup.py file used to dockerize
-
-    Returns:
-        str: The name of the image + '-sagemaker'
-    """
-
-    return make_project_image_name(setup_file_path) + "-sagemaker"
-
-
-def make_project_repository_name(docker_repository_prefix, setup_file_path):
-    return '/'.join(([docker_repository_prefix.strip('/')] if docker_repository_prefix is not None else []) + [make_project_image_name(setup_file_path)])
-
-
-def make_sagemaker_project_repository_name(docker_repository_prefix, setup_file_path):
-    return '/'.join(([docker_repository_prefix.strip('/')] if docker_repository_prefix is not None else []) + [make_sagemaker_project_image_name(setup_file_path)])
-
-
-#
-# Make run commands
-#
-
-def get_run_command_parameters(pfs):
-    remote = pfs.curr_context.remote_ctxt_url
-    if remote is not None:
-        remote = remote.replace('/{}'.format(DISDAT_CONTEXT_DIR), '')
-        local_ctxt = "{}/{}".format(pfs.curr_context.remote_ctxt, pfs.curr_context.local_ctxt)
-    else:
-        local_ctxt = "{}".format(pfs.curr_context.local_ctxt)
-    return remote, local_ctxt
-
-
-def make_run_command(
-        output_bundle,
-        output_bundle_uuid,
-        pipe_cls,
-        remote,
-        context,
-        input_tags,
-        output_tags,
-        force,
-        force_all,
-        no_pull,
-        no_push,
-        no_push_int,
-        workers,
-        pipeline_params
-):
-    """ Create a list of args.  Note that for execution via run, we always set
-    --output-bundle, even though it is optional.   The CLI and API will place a '-'
-    if the user does not specify it, which means use the default output bundle name.  Here
-    we make sure to pass it through.
-
-    Args:
-        output_bundle:
-        output_bundle_uuid:
-        pipe_cls:
-        remote:
-        context:
-        input_tags:
-        output_tags:
-        force:
-        force_all:
-        no_pull:
-        no_push:
-        no_push_int:
-        workers:
-        pipeline_params:
-
-    Returns:
-
-    """
-    args = [
-        '--output-bundle-uuid ', output_bundle_uuid,
-        '--output-bundle', output_bundle,
-        '--branch', context,
-        '--workers', str(workers)
-    ]
-    if remote:
-        args.extend(['--remote', remote])
-    if no_pull:
-        args += ['--no-pull']
-    if no_push:
-        args += ['--no-push']
-    if no_push_int:
-        args += ['--no-push-intermediates']
-    if force:
-        args += ['--force']
-    if force_all:
-        args += ['--force-all']
-    if len(input_tags) > 0:
-        for next_tag in input_tags:
-            args += ['--input-tag', next_tag]
-    if len(output_tags) > 0:
-        for next_tag in output_tags:
-            args += ['--output-tag', next_tag]
-
-    args += [str(pipe_cls)]  # The one required argument to the entrypoint
-
-    return [x.strip() for x in args + pipeline_params]
 
 
 def parse_args_tags(args_tag, to='dict'):
@@ -494,33 +243,3 @@ def slicezip(a, b):
     result[::2] = a
     result[1::2] = b
     return result
-
-
-def setup_exists(fqp_setup):
-    """ Check if file exists
-    """
-    if not os.path.exists(fqp_setup):
-        print ("No setup.py found at {}.".format(fqp_setup))
-        return False
-    return True
-
-
-def load_class(class_path):
-    """
-    Given a fully-qualified [pkg.mod.sub.classname] class name,
-    load the specified class and return a reference to it.
-
-    Args:
-        class_path (str): '.' separated module and classname
-
-    Returns:
-        class: reference to the loaded class
-    """
-    try:
-        mod_path, cls_name = class_path.rsplit('.', 1)
-    except ValueError:
-        raise ValueError('must include fully specified classpath, not local reference')
-
-    mod = importlib.import_module(mod_path)
-    cls = getattr(mod, cls_name)
-    return cls
