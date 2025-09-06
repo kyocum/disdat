e09dfbcfc310cb16c4ad878c08779085bb2428fe
Immanuel Buder <immanuel_buder@intuit.com>
2025-09-05 22:43:31 -0700

    update python version classifiers

--- setup.py
+++ setup.py
@@ -41,7 +41,11 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Operating System :: OS Independent",
         "Natural Language :: English",
     ],
